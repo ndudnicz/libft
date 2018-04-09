aced5091cab5229829079373432b7ff7913203c7
ndudnicz <ndudnicz@protonmail.com>
2018-04-09 14:06:27 +0200

    fix strdup: useless rbx

--- libftasm/src/ft_strdup.s
+++ libftasm/src/ft_strdup.s
@@ -6,20 +6,24 @@ global _ft_strdup
 section .text
 
 _ft_strdup:
-	mov rbx, rdi
-	call _ft_strlen
-	mov rdx, rax
-	inc rax
-	mov rdi, rax
-	push rax
-	call _malloc
-	cmp rax, 0
-	je fail
-	mov rdi, rax
-	mov rsi, rbx
-	pop rdx
-	call _ft_memcpy
-	ret
+cmp rdi, 0
+je fail
+; mov rbx, rdi
+call _ft_strlen
+mov rdx, rax
+inc rax
+mov rdi, rax
+push rax
+call _malloc
+cmp rax, 0
+je fail
+mov rdi, rax
+; mov rsi, rbx
+mov rsi, rdi
+pop rdx
+call _ft_memcpy
+ret
 
 fail:
-	ret
+xor rax, rax
+ret
